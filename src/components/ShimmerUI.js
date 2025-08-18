061243bf96b5ec4d873a813fdbdda4d59f582628
Sravan4350 <sravn.2345@gmail.com>
2025-08-18 22:10:22 +0530

    Added Filter functionality

---
+++ src/components/ShimmerUI.js
@@ -0,0 +1,18 @@
+const Shimmer = () => {
+  return (
+    <div className="shimmer-container">
+      {Array(30)
+        .fill("")
+        .map((_, index) => (
+          <div key={index} className="shimmer-card">
+            <div className="shimmer-img"></div>
+            <div className="shimmer-line short"></div>
+            <div className="shimmer-line medium"></div>
+            <div className="shimmer-line long"></div>
+          </div>
+        ))}
+    </div>
+  );
+};
+
+export default Shimmer;
